a6112065846e9118b2a3cf577b7ad761a1f1eb7e
noahwekesa <noahwekesa0@gmail.com>
2024-02-15 12:38:16 +0300

    frontend changes

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
-    <main>
-      <div className="container m-8">
-        <h1>Hello world</h1>
-      </div>
-    </main>
+    <div className="container m-8">
+      <h1>Hello world</h1>
+    </div>
   );
 }
